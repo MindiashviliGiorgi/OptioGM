69f6267e40bac467af22a06205bb75a62510ce97
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-10-07 01:49:20 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerListComponent } from './banner-list/banner-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : BannerListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
